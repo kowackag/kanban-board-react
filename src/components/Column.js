9cc085bccb5da7203c7c30a3d01c287d1438bea3
Kowacka Malgorzata <kowackag@gmail.com>
2021-12-06 10:11:13 +0100

    Change key in Column

--- src/components/Column.js
+++ src/components/Column.js
@@ -13,8 +13,7 @@ const Column = () => {
             <div> </div>
             <ul className ="kanban-board__column">
                 {columns.map(el=>
-            
-                <li className ="column" key = {uuid()} >
+                <li className ="column" key = {el.id} >
                 <h3 className ="column__title">{`${el.name}`}</h3>
                 <Task tasks = {tasks.filter(task => task.idColumn == el.id)}/>
                 </li>
